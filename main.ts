fe9c631790c326af574f1df5f9006738b8352711
EntaAyano <57655215+EntaAyano@users.noreply.github.com>
2023-01-17 09:49:11 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -26,9 +26,8 @@ let invader_x = 0
 let i_ = 0
 let j_ = 0
 let agent_x = 0
-let カウンター = 0
-agent_x = 2
 let point = 0
+agent_x = 2
 led.plot(agent_x, 4)
 loops.everyInterval(2000, function () {
     for (let i = 0; i <= 4; i++) {
@@ -37,16 +36,15 @@ loops.everyInterval(2000, function () {
             basic.showIcon(IconNames.Sad)
             basic.showString("" + (point))
             basic.showString("point")
-            カウンター = 0
+            led.plot(agent_x, 4)
             agent_x = 2
             point = 0
-            led.plot(agent_x, 4)
         }
     }
-    for (let k = 0; k <= 4; k++) {
-        for (let l = 0; l <= 2; l++) {
-            i_ = k * -1
-            j_ = l * -1
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= 2; j++) {
+            i_ = i * -1
+            j_ = j * -1
             if (led.point(i_ + 4, j_ + 2)) {
                 led.unplot(i_ + 4, j_ + 2)
                 led.plotBrightness(i_ + 4, j_ + 3, 50)
@@ -55,7 +53,6 @@ loops.everyInterval(2000, function () {
     }
 })
 loops.everyInterval(3000, function () {
-    カウンター += 1
     invader_x = randint(0, 4)
     led.plotBrightness(invader_x, 0, 50)
 })
